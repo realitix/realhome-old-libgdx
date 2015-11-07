fdb660e362cf53031a66f2ee8418bd428e950afc
js xps13 <realitix@gmail.com>
2015-11-07 11:42:16 +0100

    Init libgdx project

---
+++ desktop/src/com/realhome/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.realhome.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.realhome.RealHomeApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RealHomeApp(), config);
+	}
+}
