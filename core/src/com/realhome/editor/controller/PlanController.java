2fd335733f3d69d04ab574292824d1c9b6b44179
js xps13 <realitix@gmail.com>
2015-11-30 21:09:07 +0100

    Update

--- core/src/com/realhome/editor/controller/PlanController.java
+++ core/src/com/realhome/editor/controller/PlanController.java
@@ -2,6 +2,7 @@
 package com.realhome.editor.controller;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.realhome.editor.common.Message;
 import com.realhome.editor.common.pattern.mvc.BaseController;
@@ -12,7 +13,8 @@ public class PlanController extends BaseController<PlanView> {
 
 	public PlanController (PlanView view) {
 		super(view);
-		view.addListener(new PlanListener());
+		view.addListener(new PlanListener1());
+		view.addListener(new PlanListener2());
 	}
 
 	@Override
@@ -24,16 +26,24 @@ public class PlanController extends BaseController<PlanView> {
 		}
 	}
 
-	private class PlanListener extends ActorGestureListener {
+	private class PlanListener1 extends ActorGestureListener {
 		@Override
-		public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+		public void pan (InputEvent event, float x, float y, float deltaX, float deltaY) {
 			view.moveCamera(-deltaX, -deltaY);
 		}
 
 		@Override
-		public void zoom(InputEvent event, float initialDistance, float distance) {
-			System.out.println("zoom");
+		public void zoom (InputEvent event, float initialDistance, float distance) {
 			view.zoomCamera(distance);
+		}
+	}
+
+	private class PlanListener2 extends InputListener {
+		@Override
+		public boolean scrolled (InputEvent event, float x, float y, int amount) {
+			System.out.println("toto");
+			view.zoomCamera(amount);
+			return false;
 		}
 	}
 }
